22bbad45b751df0128dbbb0b16e7e730f18903f9
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-12-14 13:54:27 -0800

    Refactor

--- app/models/application_pet.rb
+++ app/models/application_pet.rb
@@ -2,10 +2,6 @@ class ApplicationPet < ApplicationRecord
   belongs_to :application
   belongs_to :pet
 
-  def approve
-    self.status = "Approved"
-  end
-
   def pending?
     status == "Pending"
   end
